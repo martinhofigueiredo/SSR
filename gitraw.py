72bac68ad48d163e39a5414f3db0c9aa5033868c
nmcnascimento <41187584+nmcnascimento@users.noreply.github.com>
2023-05-16 10:37:46 +0100

    Update gitraw.py

--- gitraw.py
+++ gitraw.py
@@ -85,7 +85,7 @@ repo_name = "SSR"
 file_path = cookies_file
 
 # Set your GitHub personal access token
-github_token = "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"
+github_token = "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"
 
 # Upload the file to GitHub
 upload_file_to_github(repo_owner, repo_name, file_path, github_token)
